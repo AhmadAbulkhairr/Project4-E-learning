f9b9b498b3d62d43d2b14353dcde1ebfcd9c3da0
AhmadAbulkhairr <ahmad.m.abulkhair@gmail.com>
2024-06-06 21:27:14 +0300

    still in the start

--- backend/routes/teachers.js
+++ backend/routes/teachers.js
@@ -1,26 +1,22 @@
-const express = require('express')
-const teacherRouter = express.Router()
+// teacherRouter.js
+const express = require('express');
+const teacherRouter = express.Router();
 
-const {teacherRegister} = require('../controllers/teachers')
+const {
+  teacherRegister,
+  getAllTeachers,
+  getTeacher,
+  updateTeacher,
+  deleteTeacher,
+} = require('../controllers/teachers');
 
 const authentication = require("../middleware/authen");
-
 const authorization = require("../middleware/author");
 
+teacherRouter.post("/register", authentication, authorization("Admin"), teacherRegister);
+teacherRouter.get("/allTeachers", getAllTeachers);
+teacherRouter.get("/Teacher/:id", getTeacher);
+teacherRouter.put("/Teacher/:id", authentication, authorization("Admin"), updateTeacher);
+teacherRouter.delete("/Teacher/:id", authentication, authorization("Admin"), deleteTeacher);
 
-
-teacherRouter.post("/register",authentication,authorization("Admin"),teacherRegister)
-teacherRouter.get("/allTeachers",getAllTeachers)
-teacherRouter.get("/Teacher/:id",getTeacher)
-teacherRouter.put("/Teacher/:id",authentication,authorization("Admin"),updateTeacher)
-teacherRouter.delete("/Teacher/:id",authentication,authorization("Admin"),deleteTeacher)
-
-
-
-
-module.exports = teacherRouter
-
-
-
-
-
+module.exports = teacherRouter;
