18097bca951a19f82789868afe45d496fb3b4a34
AhmadAbulkhairr <ahmad.m.abulkhair@gmail.com>
2024-06-07 23:52:31 +0300

    still back end

--- backend/controllers/review.js
+++ backend/controllers/review.js
@@ -1,51 +1,62 @@
-const Review = require("../models/ReviewsSchema")
+const Review = require("../models/ReviewsSchema");
 const Material = require('../models/MaterialSchema');
 
-
-
-
-// This function creates a new comment for a specific article
+// This function creates a new review for a specific material
 const createNewReview = (req, res) => {
-    const id = req.params.id;
-    const { review } = req.body;
-    const reviewer = req.token.userId;
-    const reviewerName = req.token.user
-    const newReview = new Review({
+  const id = req.params.id;
+  const { review } = req.body;
+  const reviewer = req.token.userId;
+  const reviewerName = req.token.user;
+
+  Material.findById(id)
+    .then((material) => {
+      if (!material) {
+        return res.status(404).json({
+          success: false,
+          message: 'Material not found',
+        });
+      }
+
+      const newReview = new Review({
         review,
         reviewer,
-        reviewerName
-    });
-    newReview
-      .save()
-      .then((result) => {
-        Material
-          .findByIdAndUpdate(
-            { _id: id },
-            { $push: { reviews: result._id } },
-            { new: true }
-          )
-          .then(() => {
-            res.status(201).json({
-              success: true,
-              message: `review added`,
-              review: result,
-            });
-          })
-          .catch((err) => {
-            res.status(500).json({
-              success: false,
-              message: `Server Error`,
-              err: err.message,
-            });
-          });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          success: false,
-          message: `Server Error`,
-          err: err.message,
-        });
+        reviewerName,
       });
-  };
 
-  module.exports = {createNewReview} 
+      newReview.save()
+        .then((result) => {
+          material.reviews.push(result._id);
+          material.save()
+            .then(() => {
+              res.status(201).json({
+                success: true,
+                message: 'Review added',
+                review: result,
+              });
+            })
+            .catch((err) => {
+              res.status(500).json({
+                success: false,
+                message: 'Server Error',
+                err: err.message,
+              });
+            });
+        })
+        .catch((err) => {
+          res.status(500).json({
+            success: false,
+            message: 'Server Error',
+            err: err.message,
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: 'Server Error',
+        err: err.message,
+      });
+    });
+};
+
+module.exports = { createNewReview };
